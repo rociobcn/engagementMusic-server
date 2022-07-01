e4a874ac07afc87af951bdd3fc172a45596c6f69
Rocio <roci.ramos87@gmail.com>
2022-07-01 19:39:46 +0200

    first commit

---
+++ src/test/java/com/engagementMusic/engagementMusic/EngagementMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engagementMusic.engagementMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngagementMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
